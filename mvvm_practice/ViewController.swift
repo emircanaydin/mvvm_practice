b5b3ad5af6ff12ba351dc719c118a1561778c518
emircanaydin <emircanaydin@menulux.com>
2021-09-22 01:28:22 +0300

    Initial Commit

---
+++ mvvm_practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mvvm_practice
+//
+//  Created by Emircan Aydın on 22.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
